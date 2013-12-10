a7505697487872814ef8c2edf7fa5f1a2e90ef2a
niubenaniu <niubenaniu@gmail.com>
2013-12-10 15:39:29 +0800

    rewrite xml which will be used to reply

--- www/weixin/views.py
+++ www/weixin/views.py
@@ -4,19 +4,27 @@
 
 from django.shortcuts import render_to_response,render,RequestContext
 from django.http import HttpResponse,Http404
+from django.views.decorators.csrf import csrf_exempt
 import hashlib
+import re
 
+
+@csrf_exempt
 def auto_service(request):
     '''
     support the service for 'dianshu'
     '''
     # verify the signature
-    is_access(requet)
-
-    # service
-    #auto_reply_service(request)   
+    '''
+    varify_flag = is_varified()
+    return HttpResponse(varify_flag) if varify_flag else HttpResponse('signature verify failed!')
+    '''
     
-def is_access(request):
+    # service
+    reply_msg = auto_reply_service(request)   
+    return reply_msg
+    
+def is_varified(request):
     '''
     function: check if the signature verified successful
     '''
@@ -24,8 +32,7 @@ def is_access(request):
     if (check_signature(request)):
         return HttpResponse(echostr)
     else:
-        return HttpResponse('signature verify failed!')
-
+        return None
 def check_signature(request):
     '''
     function: verify the signature
@@ -54,5 +61,43 @@ def check_signature(request):
         return False
 
 def auto_reply_service(request):
+    '''
+     
+    '''
     
-    return HtteResponse('ok')
+    # used to received text message xml
+    from_user_name_regexp = re.compile(r'<FromUserName><!\[CDATA\[(.+)\]\]></FromUserName>')
+    message_type_regexp = re.compile(r'<MsgType><!\[CDATA\[(.+)\]\]></MsgType>')
+    content_regexp = re.compile(r'<Content><!\[CDATA\[(.+)\]\]></Content>')
+    
+    if request.method == 'POST':
+        fileData=request.FILES.values()[0]
+        for chunk in fileData.chunks():
+            chunk += chunk
+    
+    form_user_name = from_user_name_regexp.search(chunk)
+    form_user_name = form_user_name.group(1)
+    message_type = message_type_regexp.search(chunk)
+    message_type = message_type.group(1)
+    content = content_regexp.search(chunk)
+    content = content.group(1)
+    
+    c = {'from_user_name':form_user_name,'message_type':message_type,'content':content}
+    print c
+        #   fp.write(chunk)
+        #fp.close()
+
+    return HttpResponse('ok')
+
+def generate_reply_xml(request):
+    generate_content(request)
+    c = {}
+    return render_to_response('book_messages.xml',c)
+
+def generate_book_content(request):
+    
+    return HttpResponse('ok')
+
+def receive_xml(request):
+    
+    return HttpResponse('ok')
