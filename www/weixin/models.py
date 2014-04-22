2511a3f8e6486a4531b05868af99f76072c4d2e5
niubenaniu <niubenaniu@gmail.com>
2014-04-22 15:57:33 +0800

    add models for article

--- www/weixin/models.py
+++ www/weixin/models.py
@@ -1,3 +1,13 @@
+#-* coding:utf-8 -*-
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.CharField(max_length=30,blank=True)
+    content = models.TextField()
+    publish_date = models.DateTimeField(auto_now_add=True)
+    
+    def __unicode__(self):
+        return self.title
+        
