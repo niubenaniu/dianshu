cd69ac8dffc841b92b170d6e02462edf729b1843
niubenaniu <niubenaniu@gmail.com>
2014-04-22 19:22:45 +0800

    change home page from static to dynamic

--- www/weixin/static/js/home_page.js
+++ www/weixin/static/js/home_page.js
@@ -1,5 +1,12 @@
 $('#bcode-img').pin();
 $('#about').tooltip();
-$('#article-dialog').on('shown.bs.modal', function (e) {
-    console.log(jQuery(e.relatedTarget).attr('ttt'));
+$('#article-dialog').on('show.bs.modal', function (e) {
+	
+	var url = '../get_article_by_id/' + $(e.relatedTarget).attr('article-id');
+    
+	$.getJSON(url,function(article){
+        	$('#article-title').text(article.title);        	
+        	$('#publish-date').text(article.publish_date);
+        	$('#article-content').text(article.content);
+    });
 });
